95415e9923c53092d23b1aad01ff26042ac79dbd
SoCalledKrish <abhiram.u.2003@gmail.com>
2025-03-03 12:11:23 +0530

    new commit

--- src/page/async.jsx
+++ src/page/async.jsx
@@ -200,7 +200,6 @@ const DownloadButton = ({ classes, report }) => {
   const awsConfig = {
     region: 'eu-west-1',
     credentials: {
-  
       accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
       secretAccessKey:process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
       sessionToken:process.env.REACT_APP_AWS_SESSION_TOKEN
